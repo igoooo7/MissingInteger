41f7cd14c181a3586c9f0e8d4c60f0d7bab0e254
Igoooo7 <igor.leskiewicz@gmail.com>
2020-04-25 15:09:52 +0200

    Initial commit

---
+++ src/com/igor/leskiewicz/Main.java
@@ -0,0 +1,37 @@
+package com.igor.leskiewicz;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       Integer[] a = new Integer[6];
+       List<Integer> al = new ArrayList<>();
+
+       a[0] = -1;
+       a[1] = 0;
+       a[2] = 1;
+       a[3] = 8;
+       a[4] = 3;
+       a[5] = 4;
+
+       al.addAll(Arrays.asList(a));
+       Collections.sort(al);
+
+       boolean done = false;
+
+        for (Integer i : al) {
+            System.out.println(i);
+
+            int num = i+1;
+            if( num > 0 && !al.contains(num) && !done) {
+                System.out.println("Missing Integer: " + num + "!");
+                done = true;
+            }
+        }
+
+
+
+    }
+}
